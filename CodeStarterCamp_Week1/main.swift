8caa64302bfc5535de91d7cd2a359f6aff8956b5
JAY-Winter <rubat0@naver.com>
2022-04-13 00:52:58 +0900

    Do implement function of STEP2
    - Week STEP2 요구사항에 따라 함수를 두 개로 나눠서 구현
    - 1. 아이스크림 부분 그리는 함수
    - 2. 막대기 부분을 그리는 함수
    
    - 1 : 아이스크림 상단부분을 직사각형의 넓이라고 생각함
    - 2 : 바 모양 부분과 바 앞 부분의 공란으로 총 2가지 파트로 생각함
    
    feat: STEP2 함수 실행부 구현

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,29 @@
 
 import Foundation
 
-print("Hello, World!")
+func makeTopOfIceCream(width: Int, height: Int) {
+    let frame = width * height
+    
+    for count in 1...frame {
+        print("*", terminator: "")
+        if count % width == 0 {
+            print("*", terminator: "\n")
+        }
+    }
+}
+// 아이스크림 그리기 - 아랫부분 만들기
+func makeIceCreamBar(frontSpace: Int, row: Int) {
+    // rowCount 는 쓰지 않으므로 "_" 처리 가능
+    for rowCount in 1...row {
+        // spaceCount 는 쓰지 않으므로 "_" 처리 가능
+        for spaceCount in 1...frontSpace {
+            print(" ", terminator: "")
+        }
+        print("| |")
+    }
+}
 
+
+// 함수 실행
+makeTopOfIceCream(width: 11, height: 8)
+makeIceCreamBar(frontSpace: 4, row: 4)
