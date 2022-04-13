e010430516c2dae4bd0c131574f8ed1b5bdb8015
hansm <hanmix2003@naver.com>
2022-04-13 23:35:43 +0900

    Step2 아이스크림 그리기 커밋

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,55 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: 1번 방법
 
+// 변수 선언
+var body: String = "***********"
+var stick: String = "    | |"
+
+// 함수 구현
+func makeIceCreamBody() {
+    for _ in 1...8 {
+        print(body)
+    }
+}
+
+func makeIceCreamStick() {
+    for _ in 1...4 {
+        print(stick)
+    }
+}
+
+// 함수 실행
+makeIceCreamBody()
+makeIceCreamStick()
+
+// MARK: 2번 방법
+
+// 변수 선언
+var body2: String = "*"
+var stick2: String = "| |"
+
+// 함수 구현
+func makeIceCreamBody2() {
+    for _ in 1...8 {
+        for _ in 1...11 {
+            print(body2, terminator: "")
+        }
+        print("")
+    }
+}
+
+func makeIceCreamStick2() {
+    for _ in 1...4{
+        for _ in 1...4 {
+            print(" ", terminator: "")
+        }
+        print(stick2)
+    }
+
+}
+
+// 함수 실행
+makeIceCreamBody2()
+makeIceCreamStick2()
