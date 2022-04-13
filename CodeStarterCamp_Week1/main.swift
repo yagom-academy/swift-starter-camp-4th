269b3c10acdf56260f46cda8af4a3b471a412ed8
ssyoun4092 <ssyoun4092@naver.com>
2022-04-13 21:52:41 +0900

    feat: 아이스크림 그릴 때 가로세로 고정값이 아닌 유동적인 값을 넣어서 그릴 수 있도록 하는 함수 구현

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,18 +1,19 @@
 import Foundation
 
-func drawIcecream() {
-    for heightStars in 1...8 {
-        for widthStars in 1...11 {
+func drawIcecream(forwidth width: Int, forheight height: Int) {
+    for heightStars in 1...height {
+        for widthStars in 1...width {
             print("*", terminator: "")
         }
         print("")
     }
 }
 
-drawIcecream()
-
-func drawIcecreamSticks() {
-    for sticks in 1...4 {
+func drawIcecreamSticks(for stickHeight: Int) {
+    for sticks in 1...stickHeight {
         print("    | |    ")
     }
 }
+
+drawIcecream(forwidth: 25, forheight: 5)
+drawIcecreamSticks(for: 5)
