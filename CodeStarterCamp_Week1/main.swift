e7f3eec9c6aa1b60445975afeaee113fa3ef32d2
Onestar <onestar7984@gmail.com>
2022-04-13 21:03:32 +0900

    comment 반영

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -12,26 +12,24 @@ let hight = 12
 let width = 11
 let halfWidth = (width/2)+1
 
-func printIceCream(){
+func printIceCream() {
     print("*", terminator: "")
 }
 
-func printStick(widthCount: Int){
-    if widthCount == halfWidth-1 || widthCount == halfWidth+1{
-        print("|", terminator: "") //막대는 폭 중간의 양옆에 위치
-    }
-    else{
+func printStick(widthPoint: Int) {
+    if widthPoint == halfWidth-1 || widthPoint == halfWidth+1 {
+        print("|", terminator: "")
+    }else {
         print(" ", terminator: "")
     }
 }
 
-for hightCount in 1...hight{
-    for widthCount in 1...width{
+for hightCount in 1...hight {
+    for widthCount in 1...width {
         if hightCount < 9 {
             printIceCream()
-        }
-        else{
-            printStick(widthCount: widthCount)
+        }else {
+            printStick(widthPoint: widthCount)
         }
     }
     print("")
