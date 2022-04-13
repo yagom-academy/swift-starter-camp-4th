82ff7f3d401d7f546eac03e57828dc896be616dd
jsa0224 <puccav@naver.com>
2022-04-13 23:51:14 +0900

    MakeIceCream

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,27 @@
 
 import Foundation
 
-print("Hello, World!")
+//매개변수 x, y를 넣었는데 왜 for문에서 인식이 안 될까요?
+//일단 _로 대체했습니다..ㅠㅠ
+
+
+
+//makeIceCream 함수를 만들어서 넣으면 실행이 안 되어요...
+//makeIceCream 부분
+
+for _ in 1...8 {
+    for _ in 1...11 {
+        print("*", terminator: "")
+    }
+    print("")
+}
+
+for _ in 1...4 {
+    for _ in 1...4 {
+        print(" ", terminator: "")
+    }
+    print("| |")
+}
+
+
 
