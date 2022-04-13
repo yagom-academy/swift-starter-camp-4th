436bd158ddb0b6989403d431d6ad7bdad68257f6
수염 <bbgg@suyeom-ui-MacBookAir.local>
2022-04-13 18:28:40 +0900

    make iceBar

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
 
+//**1
+//MARK: 아이스 만들기
+for _ in 1...8 {
+    for _ in 1...11 {
+        print("*", terminator: "")
+    }
+    print()
+}
+
+//MARK: 바 만들기
+for _ in 1...4 {
+    print("    | |")
+}
+
+//**2
+//MARK: 아이스 만들기
+func ice(count: Int) {
+    print("***********")
+}
+for count in 1...8 {
+    ice(count: count)
+}
+
+//MARK: 바 만들기
+func bar(count: Int) {
+    print("    | |")
+}
+for count in 1...4 {
+    bar(count: count)
+}
