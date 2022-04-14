4b4a95d232d67753ca5a5793e823de36e95e9155
tae97 <kim042235@naver.com>
2022-04-14 23:18:57 +0900

    아이스크림

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 
-print("Hello, World!")
+// 아이스크림 몸통 만들기
 
+func makeIceCream (){
+    for _ in 1...8 {
+        for _ in 1...11 {
+            print("*", terminator: "")
+        }
+        print("")
+    }
+}
+
+// 아이스크림 스틱 만들기
+
+func makeStick(){
+    for _ in 1...4 {
+        for _ in 1...4 {
+            print("", terminator: " ")
+        }
+        print("| |")
+    }
+}
+makeIceCream()
+makeStick()
