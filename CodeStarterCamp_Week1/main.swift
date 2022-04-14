20afa9cbe73706a159a87f3bcadee6ba719f29bb
jsa0224 <puccav@naver.com>
2022-04-14 16:26:07 +0900

    아이스크림 코드 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,27 +8,31 @@
 
 import Foundation
 
-//매개변수 x, y를 넣었는데 왜 for문에서 인식이 안 될까요?
-//일단 _로 대체했습니다..ㅠㅠ
 
-
-
-//makeIceCream 함수를 만들어서 넣으면 실행이 안 되어요...
 //makeIceCream 부분
 
-for _ in 1...8 {
-    for _ in 1...11 {
-        print("*", terminator: "")
+func makeIceCream() {
+    for height in 1...8 {
+        for width in 1...11 {
+            print("*", terminator: "")
+        }
+        print("")
     }
-    print("")
 }
 
-for _ in 1...4 {
-    for _ in 1...4 {
-        print(" ", terminator: "")
+makeIceCream()
+
+func makeIceCreamStick() {
+    for x in 1...4 {
+        for y in 1...4 {
+            print(" ", terminator: "")
+        }
+        print("| |")
     }
-    print("| |")
 }
+
+makeIceCreamStick()
+
 
 
 
