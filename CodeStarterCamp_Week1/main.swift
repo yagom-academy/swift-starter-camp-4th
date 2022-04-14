2a56cad86bdcf99a851a09929c7c67d6acd216a1
eulsaeng <zohv817@gmail.com>
2022-04-14 20:38:00 +0900

    step 2 완료

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,7 +6,24 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
 
-print("Hello, World!")
+func makeIce() {
+    let iceLayer = "**********"
+    for _ in 0...7 { print(iceLayer)}
+}
+
+func makeBar() {
+    let barLayer = "   | |   "
+    for _ in 0...3 { print(barLayer)}
+}
+
+makeIce()
+makeBar()
+
+// 두가지 함수를 한번에 호출하는 함수
+//func makeWholeIceCream() {
+//    makeIce()
+//    makeBar()
+//}
+//makeWholeIceCream()
 
