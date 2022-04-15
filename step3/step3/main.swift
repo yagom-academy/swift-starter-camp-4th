9832c9d7e304ffecb3cd239e7db5f3df79678bba
Dylan-yoon <d.dylany21@gmail.com>
2022-04-15 18:59:13 +0900

    step3 파일생성 및 함수 구성

---
+++ step3/step3/main.swift
@@ -0,0 +1,50 @@
+//
+//  main.swift
+//  step3
+//
+//  Created by 윤병희 on 2022/04/15.
+//
+
+import Foundation
+
+var length : Int = 0
+var body : String = " "
+var topping : String = " "
+var stickLength : Int = 0
+
+//길이(Int)에 따른 막대(빼빼로 손잡이)를 그리는 함수
+func handle(length : Int){
+
+}
+
+
+
+//몸통(String), 토핑(String)을 매개변수로 받아 토핑과 몸통을 그리는 함수
+func drawingBodyTopping(body : String, topping : String) {
+    
+}
+
+
+
+
+//길이(Int)에 따라 몸통을 그리는 함수
+func drawingBody(length : Int) {
+    
+}
+
+
+
+//각종 매개변수를 받아 위의 세 메서드를 호출하는 함수
+func printMethod() {
+    
+}
+
+
+
+
+
+//출력
+print("<정보> \n길이: \(length)\n몸통: \(body)  \n토핑: \(topping) \n막대길이: \(stickLength)")
+
+
+
