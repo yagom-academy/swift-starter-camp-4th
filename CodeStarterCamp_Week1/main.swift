b3d15a8f7be3590e67d4f39f4f7833afcff5d2ae
MANSU.J <seunghyun.jung.84@gmail.com>
2022-04-16 02:19:31 +0900

    Modify: 아이스크림 그리기 함수 분리
    초반에 요구사항을 제대로 확인하지 않아 한개의 함수로 아이스크림을 그리도록 했다.
    바디와 스틱으로 나누어 함수를 작성했다.
    아이스크림의 가로 값으로 위치를 구하고 값을 리턴 받아 스틱의 위치를 조정할 수 있도록 했다.

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,15 +1,6 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week1
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
-// 재활용이 가능하도록 값을 입력 받을수 있도록 작성해 보았습니다.
-func drawIcecream(_ bodyWidth: Int, _ bodyHeight: Int, _ stickHeight: Int) {
+func icecreamBody(bodyWidth: Int, bodyHeight: Int) -> Int {
 	
 	// 아이스크림 바디의 세로와 가로
 	for _ in 1...bodyHeight {
@@ -23,6 +14,12 @@ func drawIcecream(_ bodyWidth: Int, _ bodyHeight: Int, _ stickHeight: Int) {
 	let n = (bodyWidth % 2 == 0) ? 2 : 1
 	let stickPosition: Int = (bodyWidth / 2) - n
 	
+	return stickPosition
+	
+}
+
+func icecreamStick(stickPosition: Int, stickHeight: Int) {
+	
 	// 아이스크림 스틱의 세로와 위치
 	for _ in 1...stickHeight {
 		for _ in 1...stickPosition {
@@ -30,6 +27,9 @@ func drawIcecream(_ bodyWidth: Int, _ bodyHeight: Int, _ stickHeight: Int) {
 		}
 		print("| |")
 	}
+	
 }
 
-drawIcecream(11, 8, 4)
+let drawIcecreamBody = icecreamBody(bodyWidth: 11, bodyHeight: 8)
+let drawIcecreamStick: () = icecreamStick(stickPosition: drawIcecreamBody, stickHeight: 4)
+//let result2 = drawIcecreamStick(bodyWidth: result, stickHeight: 4)
