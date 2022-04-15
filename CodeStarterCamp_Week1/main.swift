6dabf05a0c9d302ff66f26e1fcfbd09e81d01c2a
jeongmin <lj845854@gmail.com>
2022-04-15 12:58:07 +0900

    refactor
    for문에 루프상수를 _로 변경, for문 외부에 상수를 선언해주고 for문의 범위를 선언했던 상수로 변경

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,8 +10,9 @@ import Foundation
 
 func drawIceCream() {
     let widthOfIceCream = 11
+    let iceCreamHeight = 8
     
-    for iceCreamHeight in 1...8 {
+    for _ in 1...iceCreamHeight {
         for _ in 1...widthOfIceCream {
             print("*", terminator: "")
         }
@@ -20,8 +21,11 @@ func drawIceCream() {
 }
 
 func drawIceCreamStick() {
-    for stickHeight in 1...4 {
-        for spaceWidth in 1...4 {
+    let stickHeight = 4
+    let spaceWidth = 4
+    
+    for _ in 1...stickHeight {
+        for _ in 1...spaceWidth {
             print(" ", terminator: "")
         }
         print("| |")
