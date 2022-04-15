15e502f4b844f94af2293355e7c78302c7850e7d
OneE <top9712@daum.net>
2022-04-16 03:44:49 +0900

    docs: Retry commit makeIcecream

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -29,10 +29,4 @@ func makeIcecreamBar(){
 makeIcecreamBody()
 makeIcecreamBar()
 
-func makeGugudan(xdan: Int){
-    for i in 1...9{
-        print(" \(xdan) x \(i) = \(xdan * i)")
-    }
-}
 
-makeGugudan(xdan: 3)
