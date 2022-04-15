03c16809739e4545ab96504ddd8ce8fee2fbaf26
Onestar <onestar7984@gmail.com>
2022-04-15 17:59:35 +0900

    test: 나만의 빼빼로 추가.

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -34,9 +34,11 @@ for hightCount in 1...hight {
     }
     print("")
 }
-
+print("")
 printPeperoOnOrder(bodyLength: 10, body: "***", topping: "", stickLength: 4)
 printPeperoOnOrder(bodyLength: 12, body: "***", topping: "&", stickLength: 4)
 printPeperoOnOrder(bodyLength: 12, body: "***", topping: "#", stickLength: 6)
 printPeperoOnOrder(bodyLength: 6, body: "|0|", topping: "", stickLength: 4)
+printPeperoOnOrder(bodyLength: 2, body: "???", topping: "", stickLength: 1)
+printPeperoOnOrder(bodyLength: 10, body: "($)", topping: "!", stickLength: 5)
 
