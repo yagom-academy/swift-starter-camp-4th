30365b9e1e153e6d198375bf2f53e1949bea034e
25bangsunu <sunnyouyaya22@gmail.com>
2022-04-15 19:24:18 +0900

    icecream

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,6 +7,20 @@
 //
 
 import Foundation
+// 아이스크림 만들기
 
-print("Hello, World!")
+func icecreamBody(icecreamElement: String) {
+    for icecreamLine in 1...8 {
+        print(icecreamElement)
+    }
+}
+
+func icecramBar(icecreamBarElement: String) {
+    for icecreamLine in 1...4 {
+        print(icecreamBarElement)
+    }
+}
+
+icecreamBody(icecreamElement: "************")
+icecramBar(icecreamBarElement: "    | |    ")
 
