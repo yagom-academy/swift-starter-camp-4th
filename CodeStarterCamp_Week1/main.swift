0a50fbfc565d77a1fb4b317c5e3e26eec9fd6c31
유연수 <yeon@yuyeonsuui-MacBookPro.local>
2022-04-15 16:16:34 +0900

    ss_4_ashyou

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-print("Hello, World!")
+//아이스크림 만들기
+func makeIceCream () {
+    for layer in 1...8 {
+        print("***********")
+    }
+}
 
+//막대기 만들기
+func makeIceCreamBar () {
+    for layer in 1...4 {
+        print("    | |")
+    }
+}
+
+//아이스크림 만들기 실행
+makeIceCream()
+makeIceCreamBar()
