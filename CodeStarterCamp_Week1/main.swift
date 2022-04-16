9597507808d1d46220e1aa80eb7fe5354dfa6f76
J-unely <cdenen20@gmail.com>
2022-04-17 02:15:08 +0900

    gitignore파일 추가, Step2( 함수와 순서도) 완성

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -5,8 +5,24 @@
 //  Created by yagom.
 //  Copyright © yagom academy. All rights reserved.
 //
-
 import Foundation
 
-print("Hello, World!")
+func iceHead(garo: Int, sero: Int) {
+    for _ in 1...sero {
+        for _ in 1...garo {print("*",terminator: "")}
+        print()
+    }
+}
 
+func iceBody(garo: Int) {
+    let i: Int = (garo-3)/2
+    for _ in 1...4 {
+        for _ in 1...i {print(" ",terminator: "")}
+        print("| |",terminator: "")
+        for _ in 1...i {print(" ",terminator: "")}
+        print()
+    }
+}
+
+iceHead(garo: 11, sero: 8)  //아이스크림 부분을 그리는 함수
+iceBody(garo: 11)           //막대기(손잡이) 부분을 그리는 함수
