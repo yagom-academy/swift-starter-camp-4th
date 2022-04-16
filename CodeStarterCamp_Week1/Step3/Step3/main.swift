f5706e293f0acc8ec748efe3ec861262bb02bd4b
lnhk98 <hozzang666@gmai.com>
2022-04-16 15:53:42 +0900

    STEP 3 코드 완성

---
+++ CodeStarterCamp_Week1/Step3/Step3/main.swift
@@ -0,0 +1,42 @@
+//
+//  main.swift
+//  Step3
+//
+//  Created by 김인호 on 2022/04/16.
+//
+
+import Foundation
+
+func makeStick(with height: Int) {
+    var i = 1
+    var j = 1
+    while i <= height {
+        j = 1
+        while j <= 2 {
+            print(" |", terminator: "")
+            j += 1
+        }
+        i += 1
+        print("")
+    }
+}
+
+func makePartOfBody(with ingredient: String, topping: String) {
+    print("\(topping)\(ingredient) ")
+    print(" \(ingredient)\(topping)")
+}
+
+func makeBody(with height: Int, using ingredient: String, topping: String) {
+    var i = 1
+    while i <= (height / 2) {
+        makePartOfBody(with: ingredient, topping: topping)
+        i += 1
+    }
+}
+
+func makePepero(with bodyHeight: Int,using ingredient: String, topping: String, stickHeight: Int) {
+    makeBody(with: bodyHeight, using: ingredient, topping: topping)
+    makeStick(with: stickHeight)
+}
+
+makePepero(with: 6, using: "|0|", topping: " ", stickHeight: 4)
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -17,18 +17,6 @@ func makeIceCreamBody() {
     }
 }
 
-//func makeStick() {
-//    for _ in 1...4 {
-//        for _ in 1...4 {
-//            print(" ", terminator: "")
-//        }
-//        for _ in 1...2 {
-//            print("| ", terminator: "")
-//        }
-//        print("")
-//    }
-//}
-
 func makeStick() {
     for _ in 1...4 {
         for _ in 1...4 {
@@ -40,4 +28,9 @@ func makeStick() {
 
 makeIceCreamBody()
 makeStick()
+
+
+
+
+
 
