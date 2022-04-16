13f40da54979e3a10c2db06e9ee06e5e6f7ce535
SEYONG OH <alaala3@gmail.com>
2022-04-16 10:55:00 +0900

    Added some files

---
+++ CodeStarterCamp_Week1/Playground.swift
@@ -0,0 +1,16 @@
+//
+//  Playground.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 오세용 on 2022/04/16.
+//
+
+import Foundation
+
+func gugu(x: Int) {
+    for count in 1...9 {
+    print(x, " * ", count, " = ", count*x)
+    }
+}
+
+// gugu(x:3)
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,30 @@
 
 import Foundation
 
-print("Hello, World!")
+
+func plusTwo(x: Int) -> Int {
+    return x + 2
+}
+
+
+func printBody() {
+    for count in 1...8 {
+        print("**********")
+    }
+}
+
+func printStick() {
+    for count in 1...4 {
+        print("    | |")
+    }
+}
+
+printBody()
+printStick()
+
+gugu(x:3)
+
+/*
+ 
+ */
 
