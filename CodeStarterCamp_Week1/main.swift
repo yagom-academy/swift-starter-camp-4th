f83b4e352f60c72365bb54a932fb0d8b3d4a037e
wecaners <wecaners@naver.com>
2022-04-18 00:51:09 +0900

    STEP2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,22 @@
 
 import Foundation
 
-print("Hello, World!")
+func iceCream(row: Int, col: Int) -> Void{
+    for _ in 1...col{
+        for _ in 1...row{
+            print("*", terminator: "")
+        }
+        print("")
+    }
+}
 
+func stick(row: Int, col: Int) -> Void{
+    for _ in 1...col{
+        for _ in 1...row{
+            print(" ", terminator: "")
+        }
+        print("| |")
+    }
+}
+iceCream(row: 11, col: 8)
+stick(row: 4, col: 4)
