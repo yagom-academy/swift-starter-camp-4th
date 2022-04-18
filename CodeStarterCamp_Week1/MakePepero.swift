0476c86588a9d261168d4d97e9fa9e9669c69a2f
Wonbi <wonbi@Wonbiui-MacBookPro.local>
2022-04-19 03:20:27 +0900

    modify drawPepero function

--- CodeStarterCamp_Week1/MakePepero.swift
+++ CodeStarterCamp_Week1/MakePepero.swift
@@ -5,77 +5,96 @@
 //  Created by Wonbi on 2022/04/17.
 //
 
+var peperoTopping: String = ""
 var peperoBodyMaterial: String = ""
-var peperoToping: String = ""
+var spaceFromTopping: String = ""
 
-func makePeperoBar(barLength: Int) {
-    for _ in 1...barLength {
-        print(" | |")
-    }
-}
-
-func drawPeperoBodyOdd(bodyMaterial: String, toping: String) {
-    peperoBodyMaterial = bodyMaterial
-    peperoToping = toping
-    if toping.isEmpty {
-        print(" \(bodyMaterial)")
+func countSpaceFromTopping() -> String {
+    spaceFromTopping = ""
+    if peperoTopping.isEmpty {
+        spaceFromTopping = " "
     } else {
-        print("\(toping)\(bodyMaterial)")
+        for _ in 1...peperoTopping.count {
+            spaceFromTopping += " "
+        }
     }
+    return spaceFromTopping
 }
 
-func drawPeperoBodyEven(bodyMaterial: String, toping: String) {
-    peperoBodyMaterial = bodyMaterial
-    peperoToping = toping
-    if toping.isEmpty {
-        print(" \(bodyMaterial)")
+func drawEvenPeperoBody() {
+    print("\(spaceFromTopping)\(peperoBodyMaterial)\(String(peperoTopping.reversed()))")
+}
+
+func drawOddPeperoBody() {
+    if peperoTopping.isEmpty {
+        print("\(spaceFromTopping)\(peperoBodyMaterial)")
     } else {
-        print(" \(bodyMaterial)\(toping)")
+        print("\(peperoTopping)\(peperoBodyMaterial)")
     }
 }
 
-func makePeperoBody(bodyLength: Int) {
-    for lengthCounter in 1...bodyLength {
+func drawPeperoBodyLength(length: Int) {
+    for lengthCounter in 1...length {
         if lengthCounter % 2 == 1 {
-            drawPeperoBodyOdd(bodyMaterial: peperoBodyMaterial, toping: peperoToping)
+            drawOddPeperoBody()
         } else {
-            drawPeperoBodyEven(bodyMaterial: peperoBodyMaterial, toping: peperoToping)
+            drawEvenPeperoBody()
         }
     }
 }
 
-func makePepero(bodyLength: Int, bodyMaterial: String, toping: String, barLength: Int) {
-    peperoBodyMaterial = bodyMaterial
-    peperoToping = toping
-    makePeperoBody(bodyLength: bodyLength)
-    makePeperoBar(barLength: barLength)
+func drawPeperoBar(length: Int) {
+    for _ in 1...length {
+        print("\(spaceFromTopping)", terminator: "")
+        if peperoBodyMaterial.count <= 2 {
+            for _ in 1...peperoBodyMaterial.count {
+                print("|", terminator: "")
+            }
+            print("")
+        } else {
+            print("|", terminator: "")
+            for _ in 1...peperoBodyMaterial.count - 2 {
+                print(" ", terminator: "")
+            }
+            print("|")
+        }
+    }
 }
 
-// 사실 이 함수로 하면 모든게 다 한번에 해결됩니다..
-func makeAllPepero(bodyLength: Int, bodyMaterial: String, toping: String, barLength: Int) {
-    var topingMaterial: String = ""                  // 토핑의 내용물이 없을 때, " "을 넣어주기 위해 만든 변수
+func drawAllPepero(bodyLength: Int, bodyMaterial: String, topping: String, barLength: Int) {
+    peperoTopping = topping
+    peperoBodyMaterial = bodyMaterial
+    spaceFromTopping = countSpaceFromTopping()
+    drawPeperoBodyLength(length: bodyLength)
+    drawPeperoBar(length: barLength)
+}
+
+
+// 이 함수로 하면 모든게 다 한번에 해결될거라 생각했지만, 유지보수하는 과정, 함수의 수정이 들어가면서 함수가 오히려 더 복잡해지네요.
+func makeAllPepero(bodyLength: Int, bodyMaterial: String, topping: String, barLength: Int) {
+    var toppingMaterial: String = ""                  // 토핑의 내용물이 없을 때, " "을 넣어주기 위해 만든 변수
     
-    if toping.isEmpty {                              // 토핑의 내용물의 유무 체크 부분
-        topingMaterial = " "
+    if topping.isEmpty {                              // 토핑의 내용물의 유무 체크 부분
+        toppingMaterial = " "
     } else {
-        topingMaterial = toping
+        toppingMaterial = topping
     }
     
     // 빼빼로 몸통 구현 부
     for bodyLengthCounter in 1...bodyLength {
         if bodyLengthCounter % 2 == 1 {             // 토핑이 좌 우로 번갈아가며 출력되도록 해주는 역할
-            print("\(topingMaterial)\(bodyMaterial)")
+            print("\(toppingMaterial)\(bodyMaterial)")
         } else {
-            for _ in 1...topingMaterial.count {     // 토핑의 길이만큼 빼빼로를 띄워주기 위해 필요한 for문
+            for _ in 1...toppingMaterial.count {     // 토핑의 길이만큼 빼빼로를 띄워주기 위해 필요한 for문
                 print(" ", terminator: "")
             }
-            print(bodyMaterial, String(topingMaterial.reversed()), separator: "")
+            print("\(bodyMaterial)\(String(toppingMaterial.reversed()))")
         }                                           // 토핑의 좌우 위치에 따라 토핑의 좌우가 반전되서 출력되도록 reversed()메소드를 활용
     }
    
     // 빼빼로 막대 구현 부
     for _ in 1...barLength {
-        for _ in 1...topingMaterial.count {         // 토핑길이 맞추기
+        for _ in 1...toppingMaterial.count {         // 토핑길이 맞추기
             print(" ", terminator: "")
         }
     
--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,69 +7,24 @@
 //
 
 
-// 함수 코딩부
-func makeIceCreamBody() {
-    for countLength in 1...8 {                // 아이스크림의 세로를 결정하는 for-in문 입니다.
-        for countWidth in 1...11 {            // 아이스크림의 가로를 결정하는 for-in문 입니다.
-            print("*", terminator: "")
-        }                                     // 매개변수 separator는 별과 별 사이의 빈칸을 주지 않게 하기 위해서 ""으로 지정했습니다.
-        print("")                             // 아이스크림 가로를 그릴 때 매개변수 terminator의 값을 ""으로 지정했기 때문에 줄바꿈용으로 넣었습니다.
-    }
-}
-
-func makeIceCreamBar() {
-    for countBar in 1...4 {                   //아이스크림 막대부분의 세로길이를 결정하는 for-in문 입니다.
-        print("    | |")
-    }
-}
-
 //함수 호출부
 makeIceCreamBody()
 makeIceCreamBar()
 
-// 여기서부터는 아이스크림의 가로와 세로를 내가 원하는 크기로 조절할 수 있게 함수를 구성해봤습니다.
-
-//함수 코딩부
-var iceCreamWidth: Int = 0                      // 아이스크림 몸통의 가로 값을 아이스크림 바 부분에서 참조해야 하기 때문에 var로 변수를 만들었습니다.
-func drawIceCreamBody(width: Int, length: Int) {
-    iceCreamWidth = width
-    for _ in 1...length {                       // 와일드 카드를 사용하라는 오류메시지가 떠서 이번에는 와일드 카드로 for문의 변수를 지정했습니다.
-        for _ in 1...width {
-            print("*", terminator: "")
-        }
-        print("")
-    }
-
-}
-
-func drawIceCreamBar() {
-    for _ in 1...4 {
-        for _ in 1...iceCreamWidth / 2 - 1 {
-            print(" ", separator: "", terminator: "")
-        }
-        if iceCreamWidth % 2 == 1 {             // 가로 별의 갯수가 짝수일 때 대칭이 안 맞는게 참을 수가 없어서 어쩔 수 없이 if문을 사용했습니다..
-            print("| |")
-        } else {
-            print("||")
-        }
-    }
-}
-
-//함수 호출부
 drawIceCreamBody(width: 10, length: 8)
 drawIceCreamBar()
 print("")
 
-makePepero(bodyLength: 10, bodyMaterial: "***", toping: "", barLength: 4)
+drawAllPepero(bodyLength: 10, bodyMaterial: "***", topping: "", barLength: 4)
 print("")
-makePepero(bodyLength: 12, bodyMaterial: "***", toping: "&", barLength: 4)
+drawAllPepero(bodyLength: 12, bodyMaterial: "***", topping: "&", barLength: 4)
 print("")
-makePepero(bodyLength: 12, bodyMaterial: "***", toping: "#", barLength: 6)
+drawAllPepero(bodyLength: 12, bodyMaterial: "***", topping: "#", barLength: 6)
 print("")
-makePepero(bodyLength: 6, bodyMaterial: "|0|", toping: "", barLength: 4)
+drawAllPepero(bodyLength: 6, bodyMaterial: "|0|", topping: "", barLength: 4)
 print("")
-makeAllPepero(bodyLength: 14, bodyMaterial: "|***|", toping: "$&", barLength: 5)
+drawAllPepero(bodyLength: 14, bodyMaterial: "|***|", topping: "$&", barLength: 5)
 print("")
-makeAllPepero(bodyLength: 8, bodyMaterial: "||", toping: "#", barLength: 3)
+drawAllPepero(bodyLength: 8, bodyMaterial: "||", topping: "#", barLength: 3)
 print("")
-makeAllPepero(bodyLength: 6, bodyMaterial: "*", toping: "", barLength: 2)
+drawAllPepero(bodyLength: 6, bodyMaterial: "*", topping: "", barLength: 2)
