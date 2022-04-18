345845d603ceb27d9b136c7950beec5bfffd534b
ssyoun4092 <ssyoun4092@naver.com>
2022-04-18 13:53:16 +0900

    feat: 각종 매개변수를 받아 위의 세 메서드를 호출하는 함수를 구현하였고, 실행예시들의 빼빼로 출력 및 커스텀한 개인빼빼로 출력까지 수행하였습니다.

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -18,5 +18,39 @@ func whatTypePepero(body: String, topping: String) {
     print(" \(body)\(topping)")
 }
 
-makePeperoStick(stickHeight: 5)
-whatTypePepero(body: "***", topping: "&")
+func peperoBodyLength(bodyLength: Int, body: String, topping: String) {
+    for _ in 1...(bodyLength / 2) {
+        whatTypePepero(body: body, topping: topping)
+    }
+}
+
+func makePepero(bodyLength: Int, body: String, topping: String, stickHeight: Int) {
+    peperoBodyLength(bodyLength: bodyLength, body: body, topping: topping)
+    makePeperoStick(stickHeight: stickHeight)
+}
+
+//makePeperoStick(stickHeight: 5)
+
+//whatTypePepero(body: "***", topping: "&")
+
+//peperoBodyLength(bodyLength: 10, body: "***", topping: "&")
+
+print("실행예시 1")
+makePepero(bodyLength: 10, body: "***", topping: " ", stickHeight: 4)
+print("")
+
+print("실행예시 2")
+makePepero(bodyLength: 12, body: "***", topping: "&", stickHeight: 4)
+print("")
+
+print("실행예시 3")
+makePepero(bodyLength: 12, body: "***", topping: "#", stickHeight: 6)
+print("")
+
+print("실행예시 4")
+makePepero(bodyLength: 6, body: "|0|", topping: " ", stickHeight: 4)
+print("")
+
+print("나만의 빼빼로")
+makePepero(bodyLength: 8, body: "|*|", topping: "$", stickHeight: 3)
+print("")
