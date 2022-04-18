bf8f40eb3148f681c1e571e28c59125ed5b6e878
MANSU.J <seunghyun.jung.84@gmail.com>
2022-04-18 16:53:48 +0900

    Style: 불필요한 줄바꿈 삭제
    코드와 코드 사이 줄바꿈을 제거

--- CodeStarterCamp_Week1/icecream.swift
+++ CodeStarterCamp_Week1/icecream.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 func drawIcecreamBody(bodyWidth: Int, bodyHeight: Int) -> Int {
-	
 	for _ in 1...bodyHeight {
 		for _ in 1...bodyWidth {
 			print("*", terminator: "")
@@ -11,20 +10,16 @@ func drawIcecreamBody(bodyWidth: Int, bodyHeight: Int) -> Int {
 	
 	let stickPositionDetail = (bodyWidth % 2 == 0) ? 2 : 1
 	let stickPosition: Int = (bodyWidth / 2) - stickPositionDetail
-	
 	return stickPosition
-	
 }
 
 func drawIcecreamStick(stickPosition: Int, stickHeight: Int) {
-	
 	for _ in 1...stickHeight {
 		for _ in 1...stickPosition {
 			print(" ", terminator: "")
 		}
 		print("| |")
 	}
-	
 }
 
 let icecreamBody = drawIcecreamBody(bodyWidth: 11, bodyHeight: 8)
