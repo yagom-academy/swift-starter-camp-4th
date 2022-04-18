055b27a538120cef95c51da157992faa311e6ce5
suyeon <higewnag@naver.com>
2022-04-19 02:16:06 +0900

    pepero 수정(함수분리)

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,12 +8,8 @@
 
 import Foundation
 
-
-
-//MARK: 실행예시
-bodyLength = 10; stickLength = 4; choco = "***"; toping = " "
-makePepero()
-
+/* 빼빼로만들기1 실행예시
+ 
 bodyLength = 12; stickLength = 4; choco = "***"; toping = "&"
 makePepero()
 
@@ -23,9 +19,38 @@ makePepero()
 bodyLength = 6; stickLength = 4; choco = "|0|"; toping = " "
 makePepero()
 
-//MARK: 내마음대로 만들기
+//
 bodyLength = 10; stickLength = 5; choco = "##"; toping = "🔥"
 makePepero()
 
 bodyLength = 12; stickLength = 5; choco = "888"; toping = "☆"
 makePepero()
+ 
+*/
+
+
+
+/*빼빼로만들기2 실행예시*/
+
+//MARK: 실행예시
+
+choco = "***"; topping = " "
+makePepero(body: 10, stick: 4)
+
+choco = "***"; topping = "&"
+makePepero(body: 12, stick: 4)
+
+choco = "***"; topping = "#"
+makePepero(body: 12, stick: 6)
+
+choco = "|0|"; topping = " "
+makePepero(body: 6, stick: 4)
+
+
+
+//MARK: 내마음대로 만들기
+choco = "##"; topping = "🔥"
+makePepero(body: 10, stick: 5)
+
+choco = "888"; topping = "☆"
+makePepero(body: 12, stick: 5)
