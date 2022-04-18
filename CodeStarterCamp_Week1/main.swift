3d12d19fa4ae284161f762d5180a0ecf157fcaed
jeongmin <lj845854@gmail.com>
2022-04-18 18:43:50 +0900

    feat : 빼빼로 만들기 추가
    drawEachBody 함수
    drawPeperoBody 함수
    drawPeperoStick 함수
    makePepero 함수
    추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,29 +8,51 @@
 
 import Foundation
 
-func drawIceCream() {
-    let widthOfIceCream = 11
-    let iceCreamHeight = 8
-    
-    for _ in 1...iceCreamHeight {
-        for _ in 1...widthOfIceCream {
-            print("*", terminator: "")
-        }
-        print()
+// STEP3
+
+
+var isToppingOnLeft = false
+
+func drawEachBody(bodyShape: String, topping: String ) -> String {
+    if isToppingOnLeft == true {
+        return topping + bodyShape
+    }
+    else {
+        return " " + bodyShape + topping
     }
 }
 
-func drawIceCreamStick() {
-    let stickHeight = 4
-    let spaceWidth = 4
-    
-    for _ in 1...stickHeight {
-        for _ in 1...spaceWidth {
-            print(" ", terminator: "")
-        }
-        print("| |")
+func drawPeperoBody(bodylength: Int, bodyShape: String, topping: String){
+    for _ in 1...bodylength {
+        isToppingOnLeft = !isToppingOnLeft
+        let layer = drawEachBody(bodyShape: bodyShape, topping: topping)
+        print(layer)
+    }
+
+}
+
+func drawPeperoStick(sticklength: Int) {
+    for _ in 1...sticklength {
+        print(" | |")
     }
 }
 
-drawIceCream()
-drawIceCreamStick()
+
+func makePepero() {
+    let bodylength: Int = 10
+    let bodyShape: String = "***"
+    var topping: String = ""
+    let sticklength: Int = 4
+    
+    print("<정보>")
+    print("길이: \(bodylength)")
+    print("몸통: \(bodyShape)")
+    print("토핑: \(topping)")
+    print("막대길이: \(sticklength)\n")
+    
+    if topping == "" { topping = " " }
+    drawPeperoBody(bodylength: bodylength, bodyShape: bodyShape, topping: topping)
+    drawPeperoStick(sticklength: sticklength)
+    
+}
+makePepero()
