972c0ce77a536d5f1d02484599f8408ea48262d4
홍승주 <hongseungju@hongseungjuui-MacBookAir.local>
2022-04-18 20:41:41 +0900

    빼빼로만들기

---
+++ CodeStarterCamp_Week1/pepero_making.swift
@@ -0,0 +1,36 @@
+//
+//  main.swift
+//  example
+//
+//  Created by 홍승주 on 2022/04/18
+//
+
+import Foundation
+
+var peperobody: String = ""
+
+func makePeperoHeigth (peperoHeight: Int) {
+    for _ in 1...peperoHeight/2 {
+        print(" " + peperobody)
+        print(" " + String(peperobody.reversed()))
+    }
+}
+func makePepero (peperoMatiral: String, topping: String) {
+    if topping.isEmpty {
+        peperobody = peperoMatiral
+    }
+    else {
+        peperobody = topping+peperoMatiral
+    }
+}
+
+func makeBar (barHeigth: Int) {
+    for _ in 1...barHeigth {
+        print(" | |")
+    }
+}
+
+makePepero(peperoMatiral: "|0|", topping: "")
+makePeperoHeigth(peperoHeight: 6)
+makeBar(barHeigth: 4)
+
