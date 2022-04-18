3871972ba211c7052adc2da16663cc140239a587
hililyy <joun406@gmail.com>
2022-04-18 14:39:28 +0900

    week01 step2 edit2

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,14 +1,14 @@
-func drawIceCream(iceCream :String){
-    for i in 0...7 {
-        for j in 0...10 {
+func drawIceCream(iceCream :String) {
+    for _ in 0...7 {
+        for _ in 0...10 {
             print(iceCream,terminator: "")
         }
         print("")
     }
 }
 
-func drawIceCreamBar(iceCreamBar :String){
-    for i in 0...3 {
+func drawIceCreamBar(iceCreamBar :String) {
+    for _ in 0...3 {
         print("    \(iceCreamBar) \(iceCreamBar)")
     }
 }
