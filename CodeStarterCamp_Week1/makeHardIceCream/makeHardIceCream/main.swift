121289c5c0c5190c6ccfc5b51ab44fdb358dabd6
shube24 <insu2405@naver.com>
2022-04-19 02:26:29 +0900

    FEAT:Fix STEP2_iceCream

---
+++ CodeStarterCamp_Week1/makeHardIceCream/makeHardIceCream/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  makeHardIceCream
+//
+//  Created by insu on 2022/04/18.
+//
+
+import Foundation
+
+func makeHardIceCream() {
+   for _ in 1...8 {
+        for _ in 1...11 {
+            print("*", terminator: "")
+        }
+        print("")
+   }
+}
+
+func hardIceCreamStick() {
+    for _ in 1...4 {
+        for _ in 1...4 {
+            print("", terminator: " ")
+        }
+        print("| |")
+    }
+}
+
+makeHardIceCream()
+hardIceCreamStick()
