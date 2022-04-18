9ef3659b422640dd4350042bb0c2ea3310ee023e
rimeorange <rimeorange83@gmail.com>
2022-04-18 21:36:01 +0900

    icecream함수 icecream.swift로 분리

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,35 +8,9 @@
 
 import Foundation
 
-func drawIcecream(width: Int, height: Int) {
-    let totalCnt: Int = width * height
-    
-    for i in 1 ... totalCnt {
-        print("*", terminator: "")
-        
-        if i % width == 0 {
-            print("")
-        }
-    }
-}
 
-func drawStick(width: Int, height: Int) {
-    let widthOddEven: Int = width % 2
-    let blankCnt: Int = (width / 2) - 1
-    
-    for _ in 1 ... height {
-        print(String(repeating: " ", count: blankCnt), terminator: "")
-        if widthOddEven == 0 {
-            print("|", "|", separator: "")
-        } else {
-            print("|" , "|", separator: " ")
-        }
-    }
-}
-
-
-
+//icecream 출력
 let widthIceCream: Int = 16
-
 drawIcecream(width: widthIceCream, height: 7)
 drawStick(width: widthIceCream, height: 4)
+
---
+++ CodeStarterCamp_Week1/pepero.swift
@@ -0,0 +1,8 @@
+//
+//  pepero.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by Won bo Shim on 2022/04/16.
+//
+
+import Foundation
