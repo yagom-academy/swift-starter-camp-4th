a3e07eda6584cd2fa058d03ea52bfde9993c55f6
유연수 <yeon@yuyeonsuui-MacBookPro.local>
2022-04-18 15:55:38 +0900

    REFACTOR STEP2 코드-flowchart 재수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,14 +9,37 @@
 import Foundation
 
 
-func makeIceCream(iceCreamLayer: Int, iceCreamBarLayer: Int) {
-    for _ in 1...iceCreamLayer {
-        print("***********")
-    }
-    for _ in 1...iceCreamBarLayer {
-        print("    | |")
+func makeIceCream(iceCreamWidth: Int, iceCreamHeight: Int, barLayer: Int) {
+    makeIceCreamBody(iceCreamWidth: iceCreamWidth, iceCreamHeight: iceCreamHeight)
+    makeIceCreamBar(iceCreamWidth: iceCreamWidth, barLayer: barLayer)
+}
+
+func makeIceCreamBody(iceCreamWidth: Int, iceCreamHeight: Int) {
+    for _ in 1...iceCreamHeight {
+        for _ in 1...iceCreamWidth {
+            print("*", separator: "", terminator: "")
+        }
+        print("")
     }
 }
 
+func makeIceCreamBar(iceCreamWidth: Int, barLayer: Int) {
+    if iceCreamWidth % 2 == 0 {
+        for _ in 1...barLayer {
+            for _ in 1...((iceCreamWidth - 4) / 2) {
+                print(" ", separator: "", terminator: "")
+            }
+            print("|  |")
+        }
+    }
+    else if iceCreamWidth % 2 == 1 {
+        for _ in 1...barLayer {
+            for _ in 1...((iceCreamWidth - 3) / 2) {
+                print(" ", separator: "", terminator: "")
+            }
+            print("| |")
+        }
+    }
+}
 
-makeIceCream(iceCreamLayer: 8, iceCreamBarLayer: 4)
+makeIceCream(iceCreamWidth: 16, iceCreamHeight: 5, barLayer: 2)
