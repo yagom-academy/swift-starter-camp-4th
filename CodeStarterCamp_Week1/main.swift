9ae3072c5c128535f72f1c92bccd5deb0e2f504e
rimeorange <rimeorange83@gmail.com>
2022-04-18 21:58:45 +0900

    draw pepero commit

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,7 +10,20 @@ import Foundation
 
 
 //icecream 출력
-let widthIceCream: Int = 16
-drawIcecream(width: widthIceCream, height: 7)
-drawStick(width: widthIceCream, height: 4)
+//let widthIceCream: Int = 16
+//drawIcecream(width: widthIceCream, height: 7)
+//drawStick(width: widthIceCream, height: 4)
 
+
+//pepero 출력
+//topping 존재
+printInfo(stickHeight: 4, bodyShape: "*****", topping: "#", bodyHeight: 9)
+
+//topping 미존재
+printInfo(stickHeight: 4, bodyShape: "***", bodyHeight: 9)
+
+//topping 존재
+printInfo(stickHeight: 4, bodyShape: "**",topping: "$", bodyHeight: 9)
+
+//topping 미존재
+printInfo(stickHeight: 4, bodyShape: "**", bodyHeight: 9)
--- CodeStarterCamp_Week1/pepero.swift
+++ CodeStarterCamp_Week1/pepero.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+
+func drawStick(bodyShape: String, stickHeignt: Int) {
+    for _ in 0 ..< stickHeignt {
+        //print("","|", String(repeating: " ", count: bodyShape.count - 2), "|")
+        print(" ", terminator: "")
+        print("|", terminator: "")
+        print(String(repeating: " ", count: bodyShape.count - 2), terminator: "")
+        print("|")
+    }
+    
+}
+
+func drawBodyShape(bodyShape: String, topping: String, toppingLR: Bool) {
+    var body: String = ""
+    var toppingShape: String = topping
+    
+    if toppingShape.isEmpty {
+        toppingShape = " "
+    }
+     
+    body = (toppingLR ? toppingShape : " ") + "\(bodyShape)" + (toppingLR ? "" : toppingShape)
+    print(body)
+}
+
+func drawBodyHeight(bodyShape: String, topping: String, bodyHeight: Int) {
+    for i in 0 ..< bodyHeight {
+        if i % 2 == 0 {
+            drawBodyShape(bodyShape: bodyShape, topping: topping, toppingLR: true)
+        } else {
+            drawBodyShape(bodyShape: bodyShape, topping: topping, toppingLR: false)
+        }
+    }
+}
+
+func printInfo(stickHeight: Int, bodyShape: String, topping: String = "", bodyHeight: Int) {
+    print("\n<정보>")
+    print("길이: \(bodyHeight)")
+    print("몸통: \(bodyShape)")
+    print("토핑: \(topping)")
+    print("막대길이: \(stickHeight)")
+    print("")
+    
+    drawBodyHeight(bodyShape: bodyShape, topping: topping, bodyHeight: bodyHeight)
+    drawStick(bodyShape: bodyShape, stickHeignt: stickHeight)
+}
