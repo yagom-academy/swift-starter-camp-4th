36ab56e538e49a399e87c36904d75fac5f0a4446
jsa0224 <puccav@naver.com>
2022-04-19 10:10:34 +0900

    빼빼로 코드 수정

--- CodeStarterCamp_Week1/MakePepero.swift
+++ CodeStarterCamp_Week1/MakePepero.swift
@@ -7,27 +7,32 @@
 
 import Foundation
  
-var peperoBodyHeight: Int = 0
-var choko: String = ""
-var sprinkle: String = ""
-var peperoStickHeight: Int = 0
-
-
-func MakeNormalPepero() {
-    for _ in 1...peperoBodyHeight {
-        print(sprinkle, choko, separator: "", terminator: "")
-        print("")
+func makeNormalPepero(bodyHeight: Int, choko: String, stickHeight: Int) {
+    for _ in 1...bodyHeight {
+        print(" ", choko, separator: "")
     }
+    
+    peperoStick(height: stickHeight)
 }
-
-func MakeToppingPepero() {
-    for _ in 1...peperoBodyHeight/2 {
+func makeToppingPepero(bodyHeight: Int, choko: String, sprinkle: String, stickHeight: Int) {
+    for _ in 1...bodyHeight/2 {
         print(sprinkle, choko, " ", "\n", " ", choko, sprinkle, separator: "")
     }
+    
+    peperoStick(height: stickHeight) // 스틱을 만드는 메서드는 메서드에 미리 포함시켜서 놓칠 일이 없도록 합니다
 }
-
-func PePeroStick() {
-    for _ in 1...peperoStickHeight {
+func peperoStick(height: Int) {
+    for _ in 1...height {
         print(" | |")
     }
+    
+    print("========================") // 구분선
+}
+// main.swift 에서는 최대한 `정의`보다는 `호출`위주로 하기 위해, 이 메서드도 여기서 미리 정의해두면 어떨까요?
+func compeletePeperos() {
+    makeNormalPepero(bodyHeight: 10, choko: "***", stickHeight: 4) // 일반 빼빼로
+    makeToppingPepero(bodyHeight: 12, choko: "***", sprinkle: "&", stickHeight: 4) // & 토핑 빼뺴로
+    makeToppingPepero(bodyHeight: 12, choko: "***", sprinkle: "#", stickHeight: 4) // # 토핑 빼빼로
+    makeNormalPepero(bodyHeight: 6, choko: "|0|", stickHeight: 4) // 누드 빼빼로
+    makeToppingPepero(bodyHeight: 10, choko: "🟫", sprinkle: "⭐︎", stickHeight: 4) // 네모 빼빼로
 }
