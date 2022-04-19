ce8b931955cd0c315a533c8001adf3e31a0df7ad
MANSU.J <seunghyun.jung.84@gmail.com>
2022-04-19 23:41:08 +0900

    Feat: 빼빼로만들기 몸통 출력
    - 빼빼로 만들기의 매개변수로 받은 몸통 출력 함수

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -9,3 +9,15 @@ func printPeperosInfo(_ bodyHeight: Int, _ bodyShape: String, _ addTopping: Stri
 """
 	print(peperosInfo, "\n")
 }
+
+func drawBodyOfPepero(body bodyHeight: Int, drawBody: String) {
+	let drawBodyOdd: String = drawBody
+	let drawBodyEven: String = String(drawBody.reversed())
+	for i in 0..<bodyHeight {
+		if i % 2 == 0 {
+			print(drawBodyOdd)
+		} else {
+			print(drawBodyEven)
+		}
+	}
+}
