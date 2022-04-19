f59afca62bf6cb93a8e792f4bd42333dbc4d18dd
25bangsunu <sunnyouyaya22@gmail.com>
2022-04-19 17:14:27 +0900

    빼빼로 몸통 및 토핑 함수

--- CodeStarterCamp_Week1/step3.swift
+++
@@ -1,6 +0,0 @@
-//
-//  step3.swift
-//  CodeStarterCamp_Week1
-//
-//  Created by 방선우 on 2022/04/18.
-//
---
+++ CodeStarterCamp_Week1/step3_plain.swift
@@ -0,0 +1,18 @@
+//
+//  step3.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 방선우 on 2022/04/19.
+//
+
+import Foundation
+
+//MARK: - 빼빼로 몸통 및 토핑
+
+func makePpaeppaeloBody(body: String, toping: String) {
+    print(
+"""
+\(toping)\(body)
+\(body)\(toping)
+""")
+}
