666dcd9be6e69887479b0b514edca6e35a8597f3
J-unely <cdenen20@gmail.com>
2022-04-19 20:56:40 +0900

    함수 이름 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,14 +7,14 @@
 //
 import Foundation
 
-func iceHead(garo: Int, sero: Int) {
+func makeIceHead(garo: Int, sero: Int) {
     for _ in 1...sero {
         for _ in 1...garo {print("*",terminator: "")}
         print()
     }
 }
 
-func iceBody(garo: Int) {
+func makeIceBong(garo: Int) {
     let i: Int = (garo-3)/2
     for _ in 1...4 {
         for _ in 1...i {print(" ",terminator: "")}
@@ -22,7 +22,7 @@ func iceBody(garo: Int) {
         for _ in 1...i {print(" ",terminator: "")}
         print()
     }
-}
+} 
 
-iceHead(garo: 11, sero: 8)  //아이스크림 부분을 그리는 함수
-iceBody(garo: 11)           //막대기(손잡이) 부분을 그리는 함수
+makeIceHead(garo: 11, sero: 8)  //아이스크림 부분을 그리는 함수
+makeIceBong(garo: 11)           //막대기(손잡이) 부분을 그리는 함수
