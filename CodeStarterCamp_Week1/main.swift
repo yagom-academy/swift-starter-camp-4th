df4b09c88f734a4db2b79872fa5242cbae6bb9a6
Much3m <kga1800@uos.ac.kr>
2022-04-19 17:29:05 +0900

    STEP2 피드백 반영

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,28 +8,27 @@
 
 import Foundation
 
-func drawIce(width: Int, height: Int) {
-    for _ in 1...height { // 파라미터로 전달받음 heigth 만큼 반복
-        for _ in 1...width { // 파라미터로 전달받은 width만큼 반복
-            print("*", terminator: "") // 개행을 방지하기 위해서 terminator: "" 사용.
+func drawIcecream(top_width: Int, top_height: Int) {
+    for _ in 1...top_height {
+        for _ in 1...top_width {
+            print("*", terminator: "")
         }
-        print() // 개행을 위한 print 사용
+        print()
     }
-    drawStick(ice_width: width) // 스틱을 출력하는 함수 호출
+    drawStick(ice_width: top_width)
 }
 
 func drawStick(ice_width: Int) {
-    for _ in 1...2 {
+    for _ in 1...4 {
         for _ in 1...(ice_width/2 - 1) {
             print(" ",terminator: "")
         }
         if ice_width % 2 == 1 {
-            print("| |") // 아이스크림의 너비가 홀수일 경우
-        }
-        else {
-            print("||") // 아이스크림의 너비가 짝수일 경우
+            print("| |")
+        } else {
+            print("||")
         }
     }
 }
 
-drawIce(width: 5, height: 8)
+drawIcecream(top_width: 11,top_height: 8)
