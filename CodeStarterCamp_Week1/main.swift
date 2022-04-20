4ac0949dc328fc966d4df0a5942b6fc80f4544be
J-unely <cdenen20@gmail.com>
2022-04-20 19:08:30 +0900

    네이밍 및 컨벤션 수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,31 +7,41 @@
 //
 import Foundation
 
-func makeIceHead(garo: Int, sero: Int) {
-    for _ in 1...sero {
-        for _ in 1...garo {print("*",terminator: "")}
+func makeIceCream(iceWidth: Int, iceHeight: Int) {
+    for _ in 1...iceHeight {
+        for _ in 1...iceWidth {
+            print("*",terminator: "")
+        }
         print()
     }
 }
 
-func makeIceBong(garo: Int) {
-    let i: Int = (garo-3) / 2
-    if garo % 2 == 0 {
+func makeIceStick(iceWidth: Int) {
+    let i: Int = (iceWidth - 3) / 2
+    if iceWidth % 2 == 0 {
         for _ in 1...4 {
-            for _ in 1...i {print(" ",terminator: "")}
+            for _ in 1...i {
+                print(" ",terminator: "")
+            }
             print("|  |",terminator: "")
-            for _ in 1...i {print(" ",terminator: "")}
+            for _ in 1...i {
+                print(" ",terminator: "")
+            }
             print()
         }
         } else {
         for _ in 1...4 {
-            for _ in 1...i {print(" ",terminator: "")}
+            for _ in 1...i {
+                print(" ",terminator: "")
+            }
             print("| |",terminator: "")
-            for _ in 1...i {print(" ",terminator: "")}
+            for _ in 1...i {
+                print(" ",terminator: "")
+            }
             print()
         }
         }
 } 
 
-makeIceHead(garo: 11, sero: 8)  //아이스크림 부분을 그리는 함수
-makeIceBong(garo: 11)           //막대기(손잡이) 부분을 그리는 함수
+makeIceCream(iceWidth: 11, iceHeight: 8)    //아이스크림 부분을 그리는 함수
+makeIceStick(iceWidth: 11)                  //막대기(손잡이) 부분을 그리는 함수
