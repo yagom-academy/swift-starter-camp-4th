a9ba80593779ef68dbbce7b7338841dcb2e9a7ef
delight010 <81559026+delight010@users.noreply.github.com>
2022-04-20 21:58:36 +0900

    아이스크림 그리기 작성

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,5 +8,47 @@
 
 import Foundation
 
-print("Hello, World!")
+func drawIceCream(){
+    let starHorizonCount = 11
+    let starVerticalCount = 8
+    var star = ""
+    var stick = ""
+    
+    func drawHard(){
+
+        for _ in 0...starVerticalCount {
+            for _ in 0...starHorizonCount {
+                star += "*"
+            }
+            star += "\n"
+        }
+    }
+
+    func drawStick(){
+        var whileCount = 0
+        let stickVerticalCount = starVerticalCount/2
+        
+        while whileCount < stickVerticalCount {
+            var count = 0
+            while count <= stickVerticalCount {
+                stick += " "
+                count += 1
+            }
+            
+            stick += "| |\n"
+            count = 0
+            whileCount += 1
+        }
+        
+    }
+    
+    drawHard()
+    drawStick()
+    
+    print(star + stick)
+
+}
+
+
+drawIceCream()
 
