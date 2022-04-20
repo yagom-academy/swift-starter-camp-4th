fc60d4d116c960cb03c7eabb1ec1f57e23fe8335
CHOSION <chosion0107@gmail.com>
2022-04-20 16:58:38 +0900

    빼빼로 손잡이 함수 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,27 +8,29 @@
 
 import Foundation
 
-func iceCream() {
-    let iceCreamWidth = 11
-    
-    for _ in 1...8 {
-        for _ in 1...iceCreamWidth {
-            print("*", terminator: "")
-        }
-        print()
-    }
-}
+// 빼빼로 길이
+print("길이: ", terminator: "")
+var peperoBodyLength = Int(readLine()!)!
+// 빼빼로 몸통
+print("몸통: ", terminator: "")
+var peperoBodyShape = readLine()!
+// 빼빼로 토핑
+print("토핑: ", terminator: "")
+var peperoTopping = readLine()!
+// 빼빼로 손잡이
+print("막대길이: ", terminator: "")
+var peperoStickLength = Int(readLine()!)!
 
-func iceCreamStick() {
-    let stickHeight = 4
+/ 빼빼로 막대 그리는 함수_(1)
+func drawPeperoStick() {
     
-    for _ in 1...stickHeight {
-        for _ in 1...4 {
+    for _ in 1 ... peperoStickLength {
+        // 빼뺴로 몸통 길이 대비 빈칸 출력(간격 조정)
+        for _ in 1 ... peperoBodyShape.count/2 {
             print(" ", terminator: "")
         }
+        // 스틱 출력
         print("| |")
     }
 }
 
-iceCream()
-iceCreamStick()
---
+++ CodeStarterCamp_Week1/pepero.swift
@@ -0,0 +1,8 @@
+//
+//  pepero.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 조세연 on 2022/04/20.
+//
+
+import Foundation
