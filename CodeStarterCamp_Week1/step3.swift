5c7abb3e9123bd352d789e86618b13c701eeff8c
SeYeong Yoon <79982120+ssyoun4092@users.noreply.github.com>
2022-04-20 15:26:57 +0900

    Update step3.swift
    Class 제거했습니다! 단순히 main파일에서 함수호출하는 것으로 사용이 가능했군요...!

--- CodeStarterCamp_Week1/step3.swift
+++ CodeStarterCamp_Week1/step3.swift
@@ -7,29 +7,26 @@
 
 import Foundation
 
-class Pepero {
-    func makePeperoStick(height: Int) {
-        for _ in 1...height {
-            print(" | | ")
-            print("")
-        }
-    }
+func makePepero(bodyLength: Int, bodyType: String, toppingType: String, stickHeight: Int) {
+    makePeperoBody(length: bodyLength, type: bodyType, toppingType: toppingType)
+    makePeperoStick(height: stickHeight)
+}
 
-    func choosePeperoBodyType(_ type: String, toppingType: String) {
-        print("\(toppingType)\(type) ")
-        print("")
-        print(" \(type)\(toppingType)")
-        print("")
+func makePeperoBody(length: Int, type: String, toppingType: String) {
+    for _ in 1...(length / 2) {
+        choosePeperoBodyType(type, toppingType: toppingType)
     }
+}
 
-    func makePeperoBody(length: Int, type: String, toppingType: String) {
-        for _ in 1...(length / 2) {
-            choosePeperoBodyType(type, toppingType: toppingType)
-        }
+func makePeperoStick(height: Int) {
+    for _ in 1...height {
+        print(" | | ")
     }
-    
-    func makePepero(bodyLength: Int, bodyType: String, toppingType: String, stickHeight: Int) {
-        makePeperoBody(length: bodyLength, type: bodyType, toppingType: toppingType)
-        makePeperoStick(height: stickHeight)
-    }
+}
+
+func choosePeperoBodyType(_ type: String, toppingType: String) {
+    print("\(toppingType)\(type) ")
+    print("")
+    print(" \(type)\(toppingType)")
+    print("")
 }
