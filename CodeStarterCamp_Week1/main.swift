14518b09e963defb9bf6f11042e14a0e9b82636a
SeYeong Yoon <79982120+ssyoun4092@users.noreply.github.com>
2022-04-20 15:28:10 +0900

    Update main.swift
    class 없이 단순히 함수호출 하도록 코드를 수정하였습니다!

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -16,22 +16,22 @@ let pepero = Pepero()
 //peperoBodyLength(bodyLength: 10, body: "***", topping: "&")
 
 print("실행예시 1")
-pepero.makePepero(bodyLength: 10, bodyType: "***", toppingType: " ", stickHeight: 4)
+makePepero(bodyLength: 10, bodyType: "***", toppingType: " ", stickHeight: 4)
 print("")
 
 print("실행예시 2")
-pepero.makePepero(bodyLength: 12, bodyType: "***", toppingType: "&", stickHeight: 4)
+makePepero(bodyLength: 12, bodyType: "***", toppingType: "&", stickHeight: 4)
 print("")
 
 print("실행예시 3")
-pepero.makePepero(bodyLength: 12, bodyType: "***", toppingType: "#", stickHeight: 6)
+makePepero(bodyLength: 12, bodyType: "***", toppingType: "#", stickHeight: 6)
 print("")
 
 print("실행예시 4")
-pepero.makePepero(bodyLength: 6, bodyType: "|0|", toppingType: " ", stickHeight: 4)
+makePepero(bodyLength: 6, bodyType: "|0|", toppingType: " ", stickHeight: 4)
 print("")
 
 print("나만의 빼빼로")
-pepero.makePepero(bodyLength: 8, bodyType: "|*|", toppingType: "$", stickHeight: 3)
+makePepero(bodyLength: 8, bodyType: "|*|", toppingType: "$", stickHeight: 3)
 print("")
 
