deae7a85a6ad9c2c200aab0c0585f6f61ac4cc8c
jsa0224 <puccav@naver.com>
2022-04-21 16:38:57 +0900

    빼빼로 코드 수정

--- CodeStarterCamp_Week1/MakePepero.swift
+++ CodeStarterCamp_Week1/MakePepero.swift
@@ -7,28 +7,7 @@
 
 import Foundation
  
-func makeNormalPepero(bodyHeight: Int, choko: String, stickHeight: Int) {
-    for _ in 1...bodyHeight {
-        print(" ", choko, separator: "")
-    }
-    
-    peperoStick(height: stickHeight)
-}
-func makeToppingPepero(bodyHeight: Int, choko: String, sprinkle: String, stickHeight: Int) {
-    for _ in 1...bodyHeight/2 {
-        print(sprinkle, choko, " ", "\n", " ", choko, sprinkle, separator: "")
-    }
-    
-    peperoStick(height: stickHeight) // 스틱을 만드는 메서드는 메서드에 미리 포함시켜서 놓칠 일이 없도록 합니다
-}
-func peperoStick(height: Int) {
-    for _ in 1...height {
-        print(" | |")
-    }
-    
-    print("========================") // 구분선
-}
-// main.swift 에서는 최대한 `정의`보다는 `호출`위주로 하기 위해, 이 메서드도 여기서 미리 정의해두면 어떨까요?
+
 func compeletePeperos() {
     makeNormalPepero(bodyHeight: 10, choko: "***", stickHeight: 4) // 일반 빼빼로
     makeToppingPepero(bodyHeight: 12, choko: "***", sprinkle: "&", stickHeight: 4) // & 토핑 빼뺴로
@@ -36,3 +15,29 @@ func compeletePeperos() {
     makeNormalPepero(bodyHeight: 6, choko: "|0|", stickHeight: 4) // 누드 빼빼로
     makeToppingPepero(bodyHeight: 10, choko: "🟫", sprinkle: "⭐︎", stickHeight: 4) // 네모 빼빼로
 }
+
+func makeNormalPepero(bodyHeight: Int, choko: String, stickHeight: Int) {
+    for _ in 1...bodyHeight {
+        print(" ", choko, separator: "")
+    }
+    
+    peperoStick(height: stickHeight)
+}
+
+func makeToppingPepero(bodyHeight: Int, choko: String, sprinkle: String, stickHeight: Int) {
+    for _ in 1...bodyHeight/2 {
+        print(sprinkle, choko, " ", "\n", " ", choko, sprinkle, separator: "")
+    }
+    
+    peperoStick(height: stickHeight)
+}
+
+func peperoStick(height: Int) {
+    for _ in 1...height {
+        print(" | |")
+    }
+    
+    print("========================") // 구분선
+}
+
+
