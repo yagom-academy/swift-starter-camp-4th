3a7d275a9a8e32b69d262dac34657e2fde1e3367
hansm <hanmix2003@naver.com>
2022-04-21 16:12:44 +0900

    Fix coding convention
    타입 선언 시 : 다음 띄어쓰기 추가

--- CodeStarterCamp_Week1/Week1Step3.swift
+++ CodeStarterCamp_Week1/Week1Step3.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-func drawPeperoBody(bodyMaterial:String, topping:String) -> String {
-    var addTopping:String = topping
+func drawPeperoBody(bodyMaterial: String, topping: String) -> String {
+    var addTopping: String = topping
     let numberOfTopping = topping.count
-    var space:String = " "
+    var space: String = " "
         
     if numberOfTopping < 1 {
         addTopping = " "
@@ -19,13 +19,13 @@ func drawPeperoBody(bodyMaterial:String, topping:String) -> String {
             space += space
         }
     }
-    let bodyShape:String = "\(addTopping)\(bodyMaterial)\(space)"
+    let bodyShape: String = "\(addTopping)\(bodyMaterial)\(space)"
     return bodyShape
 }
 
-func drawBodyLength(bodyLength:Int, drawBodyShape:String) {
-    let bodyLengthOdd:String = drawBodyShape
-    let bodyLengthEven:String = String(drawBodyShape.reversed())
+func drawBodyLength(bodyLength: Int, drawBodyShape: String) {
+    let bodyLengthOdd: String = drawBodyShape
+    let bodyLengthEven: String = String(drawBodyShape.reversed())
     for i in 0..<bodyLength {
         if i % 2 == 0 {
             print(bodyLengthOdd)
@@ -35,33 +35,33 @@ func drawBodyLength(bodyLength:Int, drawBodyShape:String) {
     }
 }
 
-func drawStickLength(stickLength:Int, topping:String, body:String) {
+func drawStickLength(stickLength: Int, topping: String, body: String) {
     for _ in 1...stickLength {
         if topping.isEmpty {
             for _ in 0...topping.count {
-                print("", terminator: " ")
+                print("", terminator:  " ")
             }
         } else {
             for _ in 1...topping.count {
-                print("", terminator: " ")
+                print("", terminator:  " ")
             }
         }
         if body.count <= 2 {
             for _ in 1...body.count {
-                print("|", terminator: "")
+                print("|", terminator:  "")
             }
             print("")
         } else {
-            print("|", terminator: "")
+            print("|", terminator:  "")
             for _ in 1...body.count - 2{
-                print(" ", terminator: "")
+                print(" ", terminator:  "")
             }
             print("|")
         }
     }
 }
 
-func combinePepero(body:String, topping:String, bodyLength:Int, stickLength:Int) {
+func combinePepero(body: String, topping: String, bodyLength: Int, stickLength: Int) {
     print("""
             [정보]
             몸통 : \(body)
@@ -70,10 +70,10 @@ func combinePepero(body:String, topping:String, bodyLength:Int, stickLength:Int)
             막대 길이 : \(stickLength)
             """, ("\n")
     )
-    let getPeperoBody = drawPeperoBody(bodyMaterial:body, topping:topping)
+    let getPeperoBody = drawPeperoBody(bodyMaterial: body, topping: topping)
     
-    drawBodyLength(bodyLength:bodyLength, drawBodyShape:getPeperoBody)
-    drawStickLength(stickLength:stickLength, topping:topping, body:body)
+    drawBodyLength(bodyLength: bodyLength, drawBodyShape: getPeperoBody)
+    drawStickLength(stickLength: stickLength, topping: topping, body: body)
     print("")
 }
 
