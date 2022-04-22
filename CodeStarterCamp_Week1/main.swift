816bb87a2866c0b59bb9c5d4372a4f46dc6e4b8f
J-unely <cdenen20@gmail.com>
2022-04-22 23:36:56 +0900

    빼빼로 초안 코드 및 설명(README)

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -7,31 +7,37 @@
 //
 import Foundation
 
-func makeIceHead(garo: Int, sero: Int) {
-    for _ in 1...sero {
-        for _ in 1...garo {print("*",terminator: "")}
-        print()
+print("""
+---------------------------------------------
+ ♥︎  빼빼로 종류(type): basic | nude
+ ♥︎  빼빼로 토핑(option): none | crunch | almond
+---------------------------------------------
+""")
+
+let (basic, nude) = ("***","|0|")
+let (none, crunch, almond, air) = (" ", "#", "&", " ")
+
+//var peperoType: String
+//var peperoOption: String
+
+
+func buildPepero(iWant peperoType: String, with peperoOption: String, sizeOf peperoLength: (choco: Int, stick: Int)){
+    let peperoLayorOdd = peperoOption + peperoType + air
+    let peperoLayorEven = air + peperoType + peperoOption
+    if peperoLength.choco % 2 == 0 {
+        for _ in 1...peperoLength.choco / 2 {
+            print(peperoLayorOdd, peperoLayorEven, separator: "\n")
+        }
+    }
+    else if peperoLength.choco % 2 != 0 {
+        for _ in 1...peperoLength.choco / 2 {
+            print(peperoLayorOdd, peperoLayorEven, separator: "\n")
+        }
+        print(peperoLayorOdd)
+    }
+    for _ in 1...peperoLength.stick {
+        print(" | | ")
     }
 }
 
-func makeIceBong(garo: Int) {
-    let i: Int = (garo-3) / 2
-    if garo % 2 == 0 {
-        for _ in 1...4 {
-            for _ in 1...i {print(" ",terminator: "")}
-            print("|  |",terminator: "")
-            for _ in 1...i {print(" ",terminator: "")}
-            print()
-        }
-        } else {
-        for _ in 1...4 {
-            for _ in 1...i {print(" ",terminator: "")}
-            print("| |",terminator: "")
-            for _ in 1...i {print(" ",terminator: "")}
-            print()
-        }
-        }
-} 
-
-makeIceHead(garo: 11, sero: 8)  //아이스크림 부분을 그리는 함수
-makeIceBong(garo: 11)           //막대기(손잡이) 부분을 그리는 함수
+buildPepero(iWant: basic, with: crunch, sizeOf: (choco: 11, stick: 3))
