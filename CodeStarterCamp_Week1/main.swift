c0eef99ba4b80b89cdf27bc970dfb37a7ad753ba
Junely <67406889+J-unely@users.noreply.github.com>
2022-04-25 17:43:40 +0900

    피드백 수정, 지난미션 백업

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -14,43 +14,50 @@ print("""
 ---------------------------------------------
 """)
 
-let (basic, nude) = ("***","|0|")
-let (none, crunch, almond, air) = (" ", "#", "&", " ")
+let (basic, nude) = ("***", "|0|")
+let (none, crunch, almond) = (" ", "#", "&")
 
-func buildFinalPepero(iWant peperoType: String, with peperoOption: String, sizeOf peperoLength: (choco: Int, stick: Int)){
-    print("<정보>", "길이: \(peperoLength.choco)", "몸통: \(peperoType)", "토핑: \(peperoOption)", "막대길이: \(peperoLength.stick)", separator: "\n", terminator: "\n\n")
-    makePeperoBody(type: peperoType, option: peperoOption, length: (choco: peperoLength.choco, stick: peperoLength.stick))
-    makePeperoStick(peperoLength: (choco: peperoLength.choco, stick: peperoLength.stick))
+func buildFinalPepero(order peperoType: String, with peperoOption: String, size peperoLength: (choco: Int, stick: Int)){
+    print("""
+    <정보>
+    길이: \(peperoLength.choco)
+    몸통: \(peperoType)
+    토핑: \(peperoOption)
+    막대길이: \(peperoLength.stick)
+    
+    """)
+    makePeperoBody(type: peperoType, option: peperoOption, length: peperoLength)
+    makePeperoStick(length: peperoLength)
     print("\n")
 }
 
 func makePeperoBody(type peperoType: String, option peperoOption: String, length peperoLength: (choco: Int, stick: Int)) {
     if peperoLength.choco % 2 == 0 {
         for _ in 1...peperoLength.choco / 2 {
-            makePeperoLayor(type: peperoType, option: peperoOption)
+            mergeOptions(type: peperoType, option: peperoOption)
         }
     }
     else if peperoLength.choco % 2 != 0 {
         for _ in 1...peperoLength.choco / 2 {
-            makePeperoLayor(type: peperoType, option: peperoOption)
+            mergeOptions(type: peperoType, option: peperoOption)
         }
-        print(peperoOption + peperoType + air)
+        print(peperoOption + peperoType + " ")
     }
 }
 
-func makePeperoLayor(type peperoType: String, option peperoOption: String) {
-    let peperoLayorOdd = peperoOption + peperoType + air
-    let peperoLayorEven = air + peperoType + peperoOption
-    print(peperoLayorOdd, peperoLayorEven, separator: "\n")
+func mergeOptions(type peperoType: String, option peperoOption: String) {
+    let oddBody = peperoOption + peperoType + " "
+    let evenBody = " " + peperoType + peperoOption
+    print(oddBody, evenBody, separator: "\n")
 }
 
-func makePeperoStick(peperoLength: (choco: Int, stick: Int)) {
+func makePeperoStick(length peperoLength: (choco: Int, stick: Int)) {
     for _ in 1...peperoLength.stick {
         print(" | | ")
     }
 }
 
-buildFinalPepero(iWant: basic, with: none, sizeOf: (choco: 10, stick: 4))   //실행 예시 1
-buildFinalPepero(iWant: basic, with: almond, sizeOf: (choco: 12, stick: 4)) //실행 예시 2
-buildFinalPepero(iWant: basic, with: crunch, sizeOf: (choco: 12, stick: 6)) //실행 예시 3
-buildFinalPepero(iWant: nude, with: none, sizeOf: (choco: 6, stick: 4))     //실행 예시 4
+buildFinalPepero(order: basic, with: none, size: (choco: 10, stick: 4))   //실행 예시 1
+buildFinalPepero(order: basic, with: almond, size: (choco: 12, stick: 4)) //실행 예시 2
+buildFinalPepero(order: basic, with: crunch, size: (choco: 12, stick: 6)) //실행 예시 3
+buildFinalPepero(order: nude, with: none, size: (choco: 6, stick: 4))     //실행 예시 4
