1fcb108feb3e8f1ae62ec174695b31c2ff4d9c34
CHOSION <chosion0107@gmail.com>
2022-04-26 14:42:07 +0900

    pepero 코드 재배열

--- CodeStarterCamp_Week1/pepero.swift
+++ CodeStarterCamp_Week1/pepero.swift
@@ -21,16 +21,14 @@ var peperoTopping = readLine()!
 print("막대길이: ", terminator: "")
 var peperoStickLength = Int(readLine()!)!
 
-let layer1 = String(peperoTopping+peperoBodyShape);
-let layer2 = String(" "+peperoBodyShape+peperoTopping);
-
+let layer1 = String(peperoTopping+peperoBodyShape)
+let layer2 = String(" "+peperoBodyShape+peperoTopping)
 
 // 빼빼로 막대 그리는 함수_(1)
 func drawPeperoStick() {
-    
-    for _ in 1 ... peperoStickLength {
+    for _ in 1...peperoStickLength {
         // 빼뺴로 몸통 길이 대비 빈칸 출력(간격 조정)
-        for _ in 1 ... peperoBodyShape.count/2 {
+        for _ in 1...peperoBodyShape.count / 2 {
             print(" ", terminator: "")
         }
         // 스틱 출력
@@ -38,21 +36,19 @@ func drawPeperoStick() {
     }
 }
 
-
 // 빼뺴로 몸통 그리기_(2)
 func drawPeperoLayer() {
     print(layer1+"\n"+layer2)
 }
 
-
 // 빼빼로 몸통 그리기_(3)
 func drawPeperoBody() {
     // 2줄 단위로 나누어 몸통 출력
-    for _ in 1 ... peperoBodyLength/2 {
+    for _ in 1...peperoBodyLength / 2 {
         drawPeperoLayer()
     }
     // 빼빼로 몸통 길이가 홀수일 때 출력
-    if peperoBodyLength%2 == 1 {
+    if peperoBodyLength % 2 == 1 {
         print(layer1)
     }
 }
