dd5d23f7a566fdd7ad8d7dcc0945f48c5ce04a2f
피우리 <piuri@piuliui-MacBookPro.local>
2022-04-26 23:55:23 +0900

    캡슐화 수정본추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,4 +8,8 @@
 
 import Foundation
 
-drawTotalPepero()
+drawTotalPepero(peperoBodyLength: 10, peperoBodyContent: " ***", peperoBodyToppingContent: " ", peperoBarLength: 4)
+drawTotalPepero(peperoBodyLength: 12, peperoBodyContent: " ***", peperoBodyToppingContent: "&", peperoBarLength: 4)
+drawTotalPepero(peperoBodyLength: 12, peperoBodyContent: " ***", peperoBodyToppingContent: "#", peperoBarLength: 6)
+drawTotalPepero(peperoBodyLength: 6, peperoBodyContent: " |0|", peperoBodyToppingContent: " ", peperoBarLength: 4)
+drawTotalPepero(peperoBodyLength: 8, peperoBodyContent: " /9/", peperoBodyToppingContent: " ", peperoBarLength: 5)
--- CodeStarterCamp_Week1/pepero.swift
+++ CodeStarterCamp_Week1/pepero.swift
@@ -24,30 +24,9 @@ func drawPeperobodyLength(length: Int) {
         }
     }
 }
-let name = "<정보>"
-var lengthOfbody: String = "길이 : "
-var peperoBody: String = "몸통 : "
-var toppings: String = "토핑 : "
-var lengthOfbar: String = "막대길이 : "
-func drawTotalPepero() {
-    //첫번째 빼빼로 출력
-    print("\(name)\n","\(lengthOfbody)10\n","\(peperoBody)***\n","\(toppings)\n","\(lengthOfbar)4\n")
-    peperobodyResult = drawPeperobodies(peperoBody: " ***", peperoTopping: " ")
-    drawPeperobodyLength(length: 10)
-    drawPeperobar(length: 4)
-    //두번째 빼빼로 출력
-    print("\n\(name)\n","\(lengthOfbody)12\n","\(peperoBody)***\n","\(toppings)&\n","\(lengthOfbar)4\n")
-    peperobodyResult = drawPeperobodies(peperoBody: " ***", peperoTopping: "&")
-    drawPeperobodyLength(length: 12)
-    drawPeperobar(length: 4)
-    //세번째 빼빼로 출력
-    print("\n\(name)\n","\(lengthOfbody)12\n","\(peperoBody)***\n","\(toppings)#\n","\(lengthOfbar)6\n")
-    peperobodyResult = drawPeperobodies(peperoBody: " ***", peperoTopping: "#")
-    drawPeperobodyLength(length: 12)
-    drawPeperobar(length: 6)
-    //네번째 빼빼로 출력
-    print("\n\(name)\n","\(lengthOfbody)6\n","\(peperoBody)|0|\n","\(toppings)\n","\(lengthOfbar)4\n")
-    peperobodyResult = drawPeperobodies(peperoBody: " |0|", peperoTopping: " ")
-    drawPeperobodyLength(length: 6)
-    drawPeperobar(length: 4)
+func drawTotalPepero(peperoBodyLength: Int, peperoBodyContent: String, peperoBodyToppingContent: String, peperoBarLength: Int) {
+    print("\n<정보>\n길이 : \(peperoBodyLength)\n몸통 : \(peperoBodyContent)\n토핑 : \(peperoBodyToppingContent)\n막대길이 : \(peperoBarLength)\n")
+    peperobodyResult = drawPeperobodies(peperoBody: peperoBodyContent, peperoTopping: peperoBodyToppingContent)
+    drawPeperobodyLength(length: peperoBodyLength)
+    drawPeperobar(length: peperoBarLength)
 }
