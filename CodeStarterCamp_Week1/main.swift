6d071f7f7e550d85ef39665995a9ba69c60e6010
이민수 <leeminsu@iminsuui-MacBookAir.local>
2022-04-27 02:59:44 +0900

    week1_step3 빼빼로 그리기 미션 다시 올림

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,32 +8,109 @@
 
 import Foundation
 
-func printIce() {
-    for count in 1...8 {
-        print("***********")
+//주석 코드는 week1_step2 아이스크림 그리기 미션 코드입니다.
+//func printIce() {
+//    for count in 1...8 {
+//        print("***********")
+//    }
+//}
+//
+//func printBar() {
+//    for count in 1...4 {
+//        print("    | |    ")
+//    }
+//}
+//
+//printIce()
+//printBar()
+//
+//func drawIce() {
+//    print("***********")
+//}
+//
+//func drawBar() {
+//    print("    | |    ")
+//}
+//
+//for star in 1...8 {
+//    drawIce()
+//}
+//for bar in 1...4 {
+//    drawBar()
+//}
+
+var bodyLength: Int = 10
+var body: String = "***"
+var topping: String = " "
+var stickLength: Int = 4
+var stick: String = "| |"
+
+func printInformation() {
+    print("<정보>")
+    print("길이: \(bodyLength)")
+    print("몸통: \(body)")
+    print("토핑: \(topping)")
+    print("막대길이: \(stickLength)")
+    print("")
+}
+
+func printStick() {
+    for _ in 1...stickLength {
+        print(" " + stick)
     }
 }
 
-func printBar() {
-    for count in 1...4 {
-        print("    | |    ")
+func drawBody() {
+        print(topping + body)
+        print(" " + body + topping)
+}
+
+func printBody() {
+    for _ in 1...bodyLength/2 {
+        drawBody()
     }
 }
 
-printIce()
-printBar()
-
-func drawIce() {
-    print("***********")
+func printPepero() {
+    printBody()
+    printStick()
 }
 
-func drawBar() {
-    print("    | |    ")
-}
+printInformation()
+printPepero()
 
-for star in 1...8 {
-    drawIce()
-}
-for bar in 1...4 {
-    drawBar()
-}
+bodyLength = 12
+body = "***"
+topping = "&"
+stickLength = 4
+stick = "| |"
+
+printInformation()
+printPepero()
+
+bodyLength = 12
+body = "***"
+topping = "#"
+stickLength = 6
+stick = "| |"
+
+printInformation()
+printPepero()
+
+bodyLength = 6
+body = "|0|"
+topping = " "
+stickLength = 4
+stick = "| |"
+
+printInformation()
+printPepero()
+
+bodyLength = 8
+body = "@@@"
+topping = "*"
+stickLength = 3
+stick = "| |"
+
+printInformation()
+printPepero()
