8626b72efdd9bf03929dab4832601e6860288ca1
이민수 <leeminsu@iminsuui-MacBookAir.local>
2022-04-28 00:30:20 +0900

    week1_step3 수정본

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,36 +8,7 @@
 
 import Foundation
 
-//주석 코드는 week1_step2 아이스크림 그리기 미션 코드입니다.
-//func printIce() {
-//    for count in 1...8 {
-//        print("***********")
-//    }
-//}
-//
-//func printBar() {
-//    for count in 1...4 {
-//        print("    | |    ")
-//    }
-//}
-//
-//printIce()
-//printBar()
-//
-//func drawIce() {
-//    print("***********")
-//}
-//
-//func drawBar() {
-//    print("    | |    ")
-//}
-//
-//for star in 1...8 {
-//    drawIce()
-//}
-//for bar in 1...4 {
-//    drawBar()
-//}
+
 
 var bodyLength: Int = 10
 var body: String = "***"
@@ -45,13 +16,16 @@ var topping: String = " "
 var stickLength: Int = 4
 var stick: String = "| |"
 
+
 func printInformation() {
-    print("<정보>")
-    print("길이: \(bodyLength)")
-    print("몸통: \(body)")
-    print("토핑: \(topping)")
-    print("막대길이: \(stickLength)")
-    print("")
+    print("""
+    <정보>
+    길이: \(bodyLength)
+    몸통: \(body)
+    토핑: \(topping)
+    막대길이: \(stickLength)
+        
+    """)
 }
 
 func printStick() {
@@ -60,14 +34,19 @@ func printStick() {
     }
 }
 
-func drawBody() {
-        print(topping + body)
-        print(" " + body + topping)
+func printBodyandTopping() {
+    print(topping + body)
+    print(" " + body + topping)
+}
+
+func drawBodyandTopping() {
+    print("\(topping)\(body)")
+    print(" \(body)\(topping)")
 }
 
 func printBody() {
     for _ in 1...bodyLength/2 {
-        drawBody()
+        printBodyandTopping()
     }
 }
 
