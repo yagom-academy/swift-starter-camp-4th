e485c8f2220a878bd98c4e86295d3e9f47d72a57
SungAh Oh <osa070@knou.ac.kr>
2022-04-28 15:54:13 +0900

    - if문 사용시 else 앞뒤 띄어쓰기 - 메소드들을 클래스 내부로 이동 - 메소드가 받는 매개변수 삭제 - 클래스 내부의 메소드로 빼빼로 출력

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -13,73 +13,74 @@ class Pepero {
     var topping: String
     var stickHeight: Int
     let stickBody: String = " | |"
-    
     init(bodyHeight: Int, body: String, topping: String, stickHeight: Int) {
         self.bodyHeight = bodyHeight
         self.body = body
         self.topping = topping
         self.stickHeight = stickHeight
     }
-}
-
-func peperoInfo(_ pepero: Pepero) {
-    let _peperoInfo =  """
-    <정보>
-    길이: \(pepero.bodyHeight)
-    몸통: \(pepero.body)
-    토핑: \(pepero.topping)
-    막대길이: \(pepero.stickHeight)\n\n
-    """
-    print(_peperoInfo)
-}
-
-// 막대 그리는 함수
-func drawPeperoStick(_ pepero: Pepero) {
-    for _ in 0..<pepero.stickHeight {
-        print("\(pepero.stickBody)")
+    
+    func peperoInfo() {
+        let _peperoInfo =  """
+        <정보>
+        길이: \(bodyHeight)
+        몸통: \(body)
+        토핑: \(topping)
+        막대길이: \(stickHeight)\n\n
+        """
+        print(_peperoInfo)
     }
-}
 
-// 토핑, 몸통 그리는 함수
-func settingToppingOnPeperoBody(_ pepero: Pepero) {
-    if pepero.topping.count > 0 {
-        pepero.body = "\(pepero.topping + pepero.body)\n \(pepero.body + pepero.topping)"
-    }else{
-        pepero.body = " \(pepero.body)"
-    }
-}
-
-// 길이에 따라 몸통을 그리는 함수
-func drawPeperoBody(_ pepero: Pepero) {
-    if pepero.topping.count > 0 {
-        for _ in 0..<(pepero.bodyHeight / 2) {
-            print(pepero.body)
-        }
-    }else{
-        for _ in 0..<pepero.bodyHeight {
-            print(pepero.body)
+    // 막대 그리는 함수
+    func drawPeperoStick() {
+        for _ in 0..<stickHeight {
+            print("\(stickBody)")
         }
     }
+
+    // 토핑, 몸통 그리는 함수
+    func settingToppingOnPeperoBody() {
+        if topping.count > 0 {
+            body = "\(topping + body)\n \(body + topping)"
+        } else {
+            body = " \(body)"
+        }
+    }
+
+    // 길이에 따라 몸통을 그리는 함수
+    func drawPeperoBody() {
+        if topping.count > 0 {
+            let halfBodyHeight = bodyHeight / 2
+            for _ in 0..<halfBodyHeight {
+                print(body)
+            }
+        } else {
+            for _ in 0..<bodyHeight {
+                print(body)
+            }
+        }
+    }
+
+    func makePepero() {
+        peperoInfo()
+        settingToppingOnPeperoBody()
+        drawPeperoBody()
+        drawPeperoStick()
+    }
 }
 
-func makePepero(_ pepero: Pepero) {
-    peperoInfo(pepero)
-    settingToppingOnPeperoBody(pepero)
-    drawPeperoBody(pepero)
-    drawPeperoStick(pepero)
-}
 
 var starPepero = Pepero(bodyHeight: 10, body: "***", topping: "", stickHeight: 4)
-makePepero(starPepero)
+starPepero.makePepero()
 
 var ampersandPepero = Pepero(bodyHeight: 12, body: "***", topping: "&", stickHeight: 4)
-makePepero(ampersandPepero)
+ampersandPepero.makePepero()
 
 var sharpPepero = Pepero(bodyHeight: 12, body: "***", topping: "#", stickHeight: 6)
-makePepero(sharpPepero)
+sharpPepero.makePepero()
 
 var nudePepero = Pepero(bodyHeight: 6, body: "|0|", topping: "", stickHeight: 4)
-makePepero(nudePepero)
+nudePepero.makePepero()
 
 var myPepero = Pepero(bodyHeight: 8, body: "***", topping: "$", stickHeight: 3)
-makePepero(myPepero)
+myPepero.makePepero()
