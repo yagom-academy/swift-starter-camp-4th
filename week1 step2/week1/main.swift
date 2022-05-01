6576120ffc35c20322ab078b4ea7377f30d2aed2
shinwooson <fresh4682@ajou.ac.kr>
2022-05-01 10:28:11 +0900

    <Fix> : WEEK1 STEP3 피드백 관련내용 수정사항
    말씀해주셨던 피드백 내용 수정사항이며 내용을 아래와 같습니다
    
    1.countingpeperotype 명사로변경
    동명사의 형태가 아닌 일반명사 형태로 바꿨습니다. 다만 단어 1개의 명사로만 표기하기엔 표현의 한계가 있어 명사 + of + 명사 의 형태로 바꿔봤습니다.
    
    2. print의 정보구현(길이: ~ , 몸통: ~, 스틱길이:, 토핑: )
    func makeicecreamof 함수에 print함수를 넣어 구현하였습니다. 처음엔 separator 와 terminator를 Print에 넣어 한줄로 구현하려 했으나 오히려 가독성이 더 떨어질꺼 같아 한줄씩 구현했습니다.
    
    3. 순서도 사진 업로드
    저번 피드백때 사진이 누락되어 재업로드 했습니다
    
    4. 문자 보간법 사용
    지적 해주셨던 func makeicecreambodyof 부분에 변수명을 내부에서 재설정하여 각 코드마다 다시 적용하여 직관성을 높였습니다.
    
    5. step2에 코드 다시 올리기
    step3 부분을 삭제 하고 기존에 제작했던 코드에 전코드를 삭제하고 step3 코드를 옮겨놨습니다.
    
    모르는 부분
    말씀해주셨던 13~15줄부분에 빼빼로 스틱부분을 분리 하라고 하신건 이해가 잘안돼서일단 그대로 놔두고 커밋했습니다.

--- week1 step2/week1/main.swift
+++ week1 step2/week1/main.swift
@@ -5,24 +5,45 @@
 //  Created by 손신우 on 2022/04/20.
 //
 
-import Foundation
+var heightOfPepero = Int(0)
 
-func makeIcecreamBodyOf(height: Int) {
-    for _ in 1 ... height{
-        print("***********")
+func makePeperoOf(bodyheight: Int, topping: String, peperotype: String, stickheight: Int) {
+    print("길이: \(bodyheight)")
+    print("몸통: \(peperotype)")
+    print("토핑: \(topping)")
+    print("막대길이: \(stickheight)", terminator: "\n\n")
+    for _ in 1 ... bodyheight {
+        makePeperoBodyOf(type: peperotype, topping: topping)
+    }
+    makePeperoStickOf(height: stickheight)
+}
+
+func makePeperoBodyOf(type: String, topping: String) {
+    heightOfPepero = heightOfPepero + 1
+    let righttopping = topping
+    let lefttopping = topping
+    let centerbodytype = type
+    if heightOfPepero % 2 == 1 {
+        print("\(lefttopping)", terminator: "")
+    }else {
+        print(" ", terminator: "")
+    }
+    print("\(centerbodytype)", terminator: "")
+    if heightOfPepero % 2 == 1 {
+        print(" ")
+    }else {
+        print("\(righttopping)")
     }
 }
 
-func makeWoodStickOf(height: Int) {
+func makePeperoStickOf(height: Int) {
     for _ in 1 ... height {
-        print("    | |")
+        print(" | |")
     }
 }
 
-func makeIcecreamOf(bodyheight:Int, stickheight: Int) {
-    makeIcecreamBodyOf(height: bodyheight)
-    makeWoodStickOf(height: stickheight)
-}
 
-makeIcecreamOf(bodyheight: 9, stickheight: 4)
-
+makePeperoOf(bodyheight: 10, topping: " ", peperotype: "***", stickheight: 4)
+makePeperoOf(bodyheight: 12, topping: "&", peperotype: "***", stickheight: 4)
+makePeperoOf(bodyheight: 12, topping: "#", peperotype: "***", stickheight: 6)
+makePeperoOf(bodyheight: 6, topping: " ", peperotype: "|0|", stickheight: 4)
