8863d09d1515def4dddd0224268d03ca7b9f98d5
MOB02 <gkswjdals111@gmail.com>
2022-05-06 16:18:02 +0900

    REFACTOR

--- CodeStarterCamp_Week1/icecream.swift
+++ CodeStarterCamp_Week1/icecream.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+func printIcecream(iceHeight: Int, stickLength: Int){
+    Icecream(height: iceHeight)
+    stick(length: stickLength)
+}
+
+func Icecream(height: Int) {
+    let ice: String = "*"
+    for _ in 1...height {
+        for _ in 1...11 {
+            print("\(ice)", terminator: "")
+        }
+        print("")
+    }
+}
+
+func stick(length: Int) {
+    let bar: String = "|"
+    for _ in 1...4 {
+        print("    \(bar) \(bar)")
+    }
+}
